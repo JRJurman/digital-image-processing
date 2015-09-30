6ae17e4e88371bef81f1df6d9583fb82f57702b8
Jesse Jurman <j.r.jurman@gmail.com>
2015-09-30 14:34:57 -0400

    before TA review

--- geo.py
+++ geo.py
@@ -3,6 +3,8 @@ IMPORTS
 """
 import numpy as np
 import ipcv
+import cv2
+
 """
 PYTHON METHOD DEFINITION
 """
@@ -24,15 +26,14 @@ def remap(src, map1, map2, interpolation=ipcv.INTER_NEAREST, borderMode=ipcv.BOR
     Returns:
         the source modified to the new mappings
     """
-
-    # first, get the max x and y value so we can build our image
-
+    pass
 
 """
 PYTHON TEST HARNESS
 """
 if __name__ == '__main__':
 
+    """
     import cv2
     import ipcv
     import os.path
@@ -59,7 +60,7 @@ if __name__ == '__main__':
     cv2.imshow(dstName, dst)
 
     ipcv.flush()
-
+    """
 """
 PYTHON METHOD DEFINITION
 """
@@ -75,7 +76,103 @@ def map_rotation_scale(src, rotation=0, scale=[1, 1]):
     Returns:
         two maps, of x values and y values
     """
-    pass
+
+    # rotation matrix (based on slides)
+    rotateMatrix = np.array([
+        np.cos(np.radians(rotation)),
+        np.sin(np.radians(rotation)),
+        -np.sin(np.radians(rotation)),
+        np.cos(np.radians(rotation))
+    ])
+    rotateMatrix = rotateMatrix.reshape(2,2)
+
+    # scale matrix (based on notes)
+    scaleMatrix = np.array([
+        1/scale[0], 0,
+        0, 1/scale[1]
+    ])
+    scaleMatrix = scaleMatrix.reshape(2,2)
+
+    # max matrix (to get the edges of dst)
+    maxMatrix = np.array([
+        scale[0], 0,
+        0, scale[1]
+    ])
+    maxMatrix = maxMatrix.reshape(2,2)
+
+    # transformations
+    transformMatrix = np.dot(rotateMatrix, scaleMatrix)
+    edgeMatrix = np.dot(rotateMatrix, maxMatrix)
+
+    print("TRANSFORM:\n", transformMatrix)
+    print("EDGE:\n", edgeMatrix)
+
+    # get the size of the Destination Map
+    corners = np.array([
+        [0,0],
+        [0, src.shape[1]],
+        [src.shape[0], 0],
+        [src.shape[0], src.shape[1]]
+    ])
+
+    # set up min and max values to compare against with
+    # the transformed corners
+    minX, minY = float('inf'), float('inf')
+    maxX, maxY = float('-inf'), float('-inf')
+
+    for xyPair in corners:
+        x, y = np.dot(edgeMatrix, xyPair)
+        minX = min(minX, x)
+        minY = min(minY, y)
+        maxX = max(maxX, x)
+        maxY = max(maxY, y)
+
+    # print("minX", minX)
+    # print("minY", minY)
+    # print("maxX", maxX)
+    # print("maxY", maxY)
+
+    # we can't have non-zero indexing, so we'll shift after the fact
+    xshift = -minX
+    yshift = -minY
+
+    xwidth = maxX + xshift
+    ywidth = maxY + yshift
+    # print("x:", np.ceil(xwidth))
+    # print("y:", np.ceil(ywidth))
+
+    xs = np.zeros((xwidth, ywidth))
+    ys = np.zeros((xwidth, ywidth))
+
+    for row in range(xs.shape[0]):
+        for col in range(xs.shape[1]):
+
+            # offset the values to center the rotation
+            offX = row - (xs.shape[0]/2)
+            offY = (xs.shape[1]/2) - col
+
+            offs = np.array([offX, offY])
+            # print("OFFS", offs)
+
+            # transform the points
+            xp, yp = np.dot(transformMatrix, offs)
+
+            # print("TRANS", [xp, yp])
+
+            # un-offset them, and place them in the final maps
+            unoffX = (xp + (src.shape[0]/2))
+            unoffY = ((src.shape[1]/2) - yp)
+
+            # print("UNOFFS", [unoffX, unoffY])
+
+            xs[row,col] = unoffX
+            ys[row,col] = unoffY
+
+
+    xs = xs.astype('float32')
+    ys = ys.astype('float32')
+    return (xs, ys)
+
 
 """
 PYTHON TEST HARNESS
@@ -93,7 +190,8 @@ if __name__ == '__main__':
     src = cv2.imread(filename)
 
     startTime = time.clock()
-    map1, map2 = ipcv.map_rotation_scale(src, rotation=30, scale=[1.3, 0.8])
+    #map1, map2 = ipcv.map_rotation_scale(src, rotation=30, scale=[1.3, 0.8])
+    map1, map2 = ipcv.map_rotation_scale(src, rotation=0, scale=[1.0, 1.0])
     elapsedTime = time.clock() - startTime
     print('Elapsed time (map creation) = {0} [s]'.format(elapsedTime))
 
@@ -139,6 +237,7 @@ PYTHON TEST HARNESS
 """
 if __name__ == '__main__':
 
+    """
     import cv2
     import ipcv
     import os.path
@@ -194,3 +293,4 @@ if __name__ == '__main__':
     cv2.imshow(dstName, dst)
 
     ipcv.flush()
+    """
